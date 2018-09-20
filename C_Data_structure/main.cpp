b011376d9638ae19a18ed7660fea7967157caccc
ChangMM <1769111903@qq.com>
2018-09-21 03:03:22 +0800

    添加函数相关的知识点的整理，包括构造函数、析构函数、内联函数

--- C_Data_structure/main.cpp
+++ C_Data_structure/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class Sample{
+private:
+    int x;
+public:
+    Sample(int i){
+        x = i;
+        cout<<"x="<<x<<" constructor called"<<endl;
+    }
+    ~Sample(){
+        cout<<"x="<<x<<" destructor called"<<endl;
+    }
+};
+
+Sample s1(1);
+void func(){
+    cout<<"func start"<<endl;
+    static Sample s2(2);
+    Sample s3(3);
+    cout<<"func end"<<endl;
+}
+int main(){
+    cout<<"main start"<<endl;
+    Sample s4(4);
+    s4 = 6;
+    {
+    Sample s5(5);
+    }
+    func();
+    cout<<"main end"<<endl;
     return 0;
 }
